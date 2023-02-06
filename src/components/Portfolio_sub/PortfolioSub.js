de0a2ff63bb79c8f3eb2cc55679f6016cf187336
kim-hyeona <kim-hyeona991113@github.com>
2023-02-06 18:05:34 +0900

    포트폴리오 설명페이지

--- src/components/Portfolio_sub/PortfolioSub.js
+++ src/components/Portfolio_sub/PortfolioSub.js
@@ -6,10 +6,36 @@ function PortfolioSub(props) {
         <div className='portfolio_sub'>
         <h2>포트폴리오 제작 과정</h2>
 
+        <h3># 목표</h3>
         <ul>
             <li></li>
             <li></li>
         </ul>
+
+        <h3># 디렉토리 구조</h3>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+
+        <h3># 사용기술</h3>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+
+        <h3># 페이지 구성</h3>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+
+        <h3># 문제 및 해결</h3>
+        <ul>
+            <li></li>
+            <li></li>
+        </ul>
+        
         </div>
     );
 }
